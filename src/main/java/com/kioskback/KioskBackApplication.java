856db270e4820f20fdce4deadb73b910e7bce30a
leejuan <wndks0886@metabuild.co.kr>
2023-09-12 20:51:52 +0900

    first commit

---
+++ src/main/java/com/kioskback/KioskBackApplication.java
@@ -0,0 +1,13 @@
+package com.kioskback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KioskBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KioskBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kioskback/KioskBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kioskback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KioskBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
